f7cdd7e45865e4594beec70088ab8958afe9cf73
Juho Timonen <juho.timonen@aalto.fi>
2020-07-14 01:21:43 +0300

    complete part 3a

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,28 +1,86 @@
 const express = require('express')
+var morgan = require('morgan')
 const app = express()
+app.use(express.json())
+morgan.token('data', function (req, res) {
+    var out = ' '
+    if(req.method === 'POST'){
+        out = JSON.stringify(req.body)
+    }
+    return out 
+})
 
-let notes = [
-    { id: 1, content: "HTML is easy", date: "2020-01-10T17:30:31.098Z", important: true },
-    { id: 2, content: "Browser can execute only Javascript", date: "2020-01-10T18:39:34.091Z", important: false },
-    { id: 3, content: "GET and POST are the most important methods of HTTP protocol", date: "2020-01-10T19:20:14.298Z", important: true }
+
+app.use(morgan(':method :url :status - :response-time ms :data'))
+
+let persons = [
+    { id: 1, name: "Seppo Kauranen", number: "123-123-123" },
+    { id: 2, name: "Mary Poppendieck", number: "044-32-22200" },
+    { id: 3, name: "Juho Timonen", number: "20-12-3253" }
 ]
 
+// Routes
 app.get('/', (req, res) => {
-    res.send('<h1>Hello World!</h1>')
+    res.send('<h1>Hello Worldd!</h1>')
 })
 
-app.get('/api/notes', (req, res) => {
-    res.json(notes)
+app.get('/api/persons', (req, res) => {
+    res.json(persons)
 })
 
-app.get('/api/notes/:id', (request, response) => {
+app.get('/info', (req, res) => {
+    const n = persons.length
+    let content = '<h1>Info</h1>'
+    content += `<p>The phonebook has info about ${n} persons.<p>`
+    content += '<p>' + (new Date()).toString() + '<p>'
+    res.send(content)
+})
+
+
+// GET
+app.get('/api/persons/:id', (request, response) => {
     const id = Number(request.params.id)
-    const note = notes.find(note => note.id === id)
-    response.json(note)
-  })
+    const person = persons.find(person => person.id === id)
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+})
 
+// DELETE
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+    response.status(204).end()
+})
+
+const generateId = () => {
+    const maxId = persons.length > 0
+        ? Math.max(...persons.map(n => n.id))
+        : 0
+    return maxId + 1
+}
+
+// POST
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+    if (!body.name) { return response.status(400).json({error: 'name missing'}) }
+    if (!body.number) { return response.status(400).json({error: 'number missing'}) }
+    const newPerson = {
+        name: body.name,
+        number: body.number,
+        id: generateId(),
+    }
+    const sameName = persons.filter(person => person.name === newPerson.name)
+    if(sameName.length > 0){ return response.status(400).json({error: 'name already in phonebook!'}) }
+    persons = persons.concat(newPerson)
+    response.json(newPerson)
+})
+
+// LISTEN
 const PORT = 3001
 app.listen(PORT, () => {
-    console.log(`Server running on p o r t ${PORT}`)
+    console.log(`Server running on port ${PORT}`)
 })
 
