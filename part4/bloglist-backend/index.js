e1fa54b27a79005b5e203737aa5b51743d58c59c
Juho Timonen <juho.timonen@aalto.fi>
2020-07-16 23:55:50 +0300

    add deps

--- part4/bloglist-backend/index.js
+++ part4/bloglist-backend/index.js
@@ -5,36 +5,36 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 
 const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
+    title: String,
+    author: String,
+    url: String,
+    likes: Number
 })
 
 const Blog = mongoose.model('Blog', blogSchema)
 
 const mongoUrl = 'mongodb://localhost/bloglist'
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 
 app.use(cors())
 app.use(express.json())
 
 app.get('/api/blogs', (_, response) => {
-  Blog.find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
+    Blog.find({})
+        .then(blogs => {
+            response.json(blogs)
+        })
 })
 
 app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-  blog.save()
-    .then(result => {
-      response.status(201).json(result)
-    })
+    const blog = new Blog(request.body)
+    blog.save()
+        .then(result => {
+            response.status(201).json(result)
+        })
 })
 
 const PORT = 3001
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+    console.log(`Server running on port ${PORT}`)
 })
